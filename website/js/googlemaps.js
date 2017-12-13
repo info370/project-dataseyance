4d7197d46ff6c5c3ca21e10dc84e047dce469a6e
Ishan Saksena <ishansaksena@gmail.com>
2017-12-13 13:04:20 -0800

    Added map

--- website/js/googlemaps.js
+++ website/js/googlemaps.js
@@ -1,1 +1,9 @@
-$(function () {})
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 47.6062, lng: -122.3321},
+        zoom: 11
+    });
+
+    // Add markers of excess land plots. 
+}
